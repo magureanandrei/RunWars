59eed46df1de3f34961e645960257ee529d8f40e
Raul1963 <raulmarginean1963@gmail.com>
2025-11-08 20:39:31 +0200

    implemented login to check with db for user

--- app/src/main/java/tech/titans/runwars/services/UserService.kt
+++ app/src/main/java/tech/titans/runwars/services/UserService.kt
@@ -8,7 +8,7 @@ import tech.titans.runwars.repo.UserRepo
 
 object UserService {
 
-    fun addUser(firstName: String, lastName: String, userName: String, email: String, password: String){
+    fun signUpUser(firstName: String, lastName: String, userName: String, email: String, password: String){
         val auth = FirebaseAuth.getInstance()
         auth.createUserWithEmailAndPassword(email,password)
             .addOnCompleteListener{ task ->
@@ -28,12 +28,24 @@ object UserService {
                     UserRepo.addUser(user)
                 }
                 else{
-                    Log.e("UserService: addUser: ", "Uid is null")
+                    Log.e("UserService: signUpUser: ", "Uid is null")
                     throw NullPointerException("Uid is null")
                 }
             }
             else{
-                Log.e("UserService: addUser: ", "Task was not successful")
+                Log.e("UserService: signUpUser: ", "Task was not successful")
+            }
+        }
+    }
+    fun loginUser(email: String, password: String, onResult: (Boolean, String?) -> Unit){
+        val auth = FirebaseAuth.getInstance()
+        auth.signInWithEmailAndPassword(email,password).addOnCompleteListener { task ->
+            if(task.isSuccessful){
+                onResult(true,null)
+                Log.i("UserService: loginUser: ", "User logged in")
+            }
+            else{
+                onResult(false, task.exception?.message)
             }
         }
     }
