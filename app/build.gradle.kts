fde735a9915d6520cb7cad3b2266d98739c80638
magureanandrei <andimagurean714@gmail.com>
2025-11-11 18:21:50 +0200

    initial map part commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "tech.titans.runwars"
-        minSdk = 29
+        minSdk = 26
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
